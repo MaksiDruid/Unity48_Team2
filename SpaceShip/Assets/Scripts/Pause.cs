5af124e80faef14555992fc2a2e6c6eb145927aa
nesBRUTALKOT <rifaromarom@gmail.com>
2024-03-03 12:50:34 +0500

    pausa
    pausa na Esc + metiotiti

---
+++ SpaceShip/Assets/Scripts/Pause.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class QuestWindow : MonoBehaviour
+{
+    private bool isOn;
+    public GameObject Pause;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isOn = !isOn;
+            Pause.SetActive(isOn);
+            Time.timeScale = 0;
+            if (isOn)
+            {
+                
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                Time.timeScale = 1;
+                
+            }
+        }
+    }
+}
